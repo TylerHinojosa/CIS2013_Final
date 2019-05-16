68a8125ecc3d940935fe510c7a15d9accce09cfc
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-05-15 20:18:00 -0500

    My Final

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	// Favorite part of class
+	cout << "GitHub to submit my assignments than using Pearson cause that website is a dumpster fire" << endl;
+	
+	// Least favorite part of class
+	cout << "While I understand the reason behind it, when you mentioned assignments but don't post any does get on my nerves" << endl;
+	
+	// What could be changed to make the class better?
+	cout << "I think it would be better if your allow your students to work on a random assignment than choose one they should do cause like you said it took way too much of your time and you end up not being able to do submit it" << endl;
+	
+	return 0;
+}
+	
